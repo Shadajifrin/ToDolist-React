537b846d4d72257e850a44cae4ae2be0962dcd0b
Shadajifrin <fathimashadanp@gmail.com>
2023-11-02 21:37:41 +0530

    functionalities given

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
 import { useState } from 'react'
 import './App.css'
+import { IoMdDoneAll } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
 
 function App() {
   const [todos, setTodos] = useState([])
   const [todo, setTodo] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  const addTodo = () => {
+    setTodos([...todos, {list:todo,id:Date.now(),status:false}]);
+    console.log(todos)
+    setTodo('')
+  }
+  const onDelete=(id)=>{
+    setTodos( todos.filter((todo)=>todo.id !== id))
+  }
+  const onComplete=(id)=>{
+    let complete=todos.map((list)=>{
+      if(list.id===id)
+      {
+      return({...list,status: !list.status})
+      }
+      return list
+    })
+    setTodos(complete)
+  }
   return (
     <div className="app" >
-      <header>
-        <h1> Shada's ToDo List</h1>
-      </header>
-      <div className="todo-input">
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-      </div>
-      <button className="todo-button" type="submit">
-        <i onClick={() => setTodos([...todos, todo])} className="fa fa-plus"></i>
-      </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="incomplete">Incomplete</option>
-        </select>
-      </div>
+      <h1> Shada's ToDo List</h1>
+      <form className='todo-input' onSubmit={handleSubmit}>
+        <input type="text" value={todo} className='form-control' placeholder='Enter your todos' onChange={(e) => setTodo(e.target.value)} />
+        <button className="todo-button" type="submit">
+          <i onClick={addTodo} className="fa fa-plus"></i>
+        </button>
+      </form>
       <div className="todos">
-        {todos.map((value) => {
-          return (<div className="todo">
-            <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>{value}</p>
-            </div>
-            <div className="right">
-              <i className="fas fa-times"></i>
-            </div>
-          </div>)
-        })}
+        <ul>
+          {
+            todos.map((todo) => (
+              <li className='list-items'>
+                <div className='lists' id={todo.status ? "list-item": ''}> {todo.list}</div>
+                <span>
+                  <IoMdDoneAll className="list-icons" id="complete" title='Complete' onClick={()=>onComplete(todo.id)}/>
+                  <MdDelete className="list-icons" id="delete" title='Delete' onClick={()=>onDelete(todo.id)} />
+                </span>
+
+              </li>
+            ))
+
+          }
+
+
+        </ul>
       </div>
     </div >
   );
